2f9d86b228477b0bd481b1ede456e4e6d82fc6f3
adalek <61150101+adalek@users.noreply.github.com>
2021-08-28 15:00:29 +0400

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -40,7 +40,7 @@ export const Footer: React.FC<{
         </div>
       ) : null}
 
-      <div className={styles.social}>
+      <div className={styles.social} style='padding-left: 10%'>
         {config.twitter && (
           <a
             className={styles.twitter}
