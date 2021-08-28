1a2b435f06e478351e4f209a64e38c06c53a5e59
adalek <61150101+adalek@users.noreply.github.com>
2021-08-28 09:45:46 +0400

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,6 +8,7 @@ export default class MyDocument extends Document {
       <IconContext.Provider value={{ style: { verticalAlign: 'middle' } }}>
         <Html lang='en'>
           <Head>
+            <link rel='icon' href='/fishcon-16x16.png' />
             <link rel='shortcut icon' href='/fishcon-16x16.png' />
 
             <link
